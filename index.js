6343a35fbff8c519717cdc8faa40af71721d5e29
sweqtyy <58605906+sweqtyy@users.noreply.github.com>
2021-09-17 15:07:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on("message", async message => {
     if (cmd === "credits") return message.channel.send(`credits go to <@271387672986124289> for coding the bot, and random ppl who helped find the way to use discord activities. <3`);
     if (cmd === "invite") return message.channel.send(`You may invite the bot using the link here:\nhttps://discord.com/oauth2/authorize?client_id=751195834468532296&permissions=240519605457&scope=bot`);
     if (cmd === "resetbot") {
-        if (!message.author.id === "271387672986124289" ) {
+        if (message.author.id != "271387672986124289") {
              message.channel.send(`Resetting bot..`);
             client.destroy().then(() => {
                 client.login(process.env.TOKEN)
