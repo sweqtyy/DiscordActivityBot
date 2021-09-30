8db7d0d8795723fe7ccec3c98ff53dd341f791db
will <58605906+sweqtyy@users.noreply.github.com>
2021-09-29 20:25:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const client = new Client();
 const PREFIX = "-";
 const BotOwnerID = "271387672986124289"
-const BotVersion = "v1.2"
+const BotVersion = "v1.3"
 const ACTIVITIES = {
     "poker": {
         id: "755827207812677713", 
